20449d012ed15590ca6370f66100c3ac5699adef
BearB34R <andynguyendo@csu.fullerton.edu>
2024-05-23 16:43:42 -0700

    finished hero page currently working on about me

---
+++ src/components/About/About.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About Me!</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Fullstack Developer</h3>
+              <p>
+                I'm a fullstack developer with experience in designing, building
+                and maintaining web applications.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Servers</h3>
+              <p>
+                I have experience setting up home servers and have worked with
+                databases and cloud networking.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Learning</h3>
+              <p>
+                Given enough time and resources, I can learn and adapt to new
+                technologies and frameworks quickly and efficiently.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I'm Andy</h1>
-        <p>
-          I'm a full-stack developer looking for opportunities to learn and
-          improve. Reach out if you'd like to learn more!
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Andy</h1>
+        <p className={styles.description}>
+          I'm a Computer Science student studying at Cal State Fullerton.
         </p>
+        <p className={styles.description}>
+          Always learning and looking for new opportunities. Contact me to learn
+          more!
+        </p>
+        <a href="andynguyendo@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
