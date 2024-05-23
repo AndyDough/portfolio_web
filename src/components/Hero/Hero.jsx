9da50c744190d8611d814afc322617a536a6fc4d
BearB34R <andynguyendo@csu.fullerton.edu>
2024-05-22 17:54:37 -0700

    finished navbar and started on Hero component:

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hi, I'm Andy</h1>
+        <p>
+          I'm a full-stack developer looking for opportunities to learn and
+          improve. Reach out if you'd like to learn more!
+        </p>
+      </div>
+    </section>
+  );
+};
