ad3b87a8d833886bc508d1e58d91fbab0d1a2309
BearB34R <andynguyendo@csu.fullerton.edu>
2024-05-28 02:23:52 -0700

    redid heroImage animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>About Me!</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
+          src={getImageUrl("about/serverAnimation.gif")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
